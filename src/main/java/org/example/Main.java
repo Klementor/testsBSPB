0741899986ddec7f1d117d68952b505d9e5129fe
Klementor <mihailkrylov8189@gmail.com>
2023-11-02 10:53:27 +0300

    experiments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.lang.model.element.Element;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mihai\\IdeaProjects\\testsBSPB\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.bspb.ru/");
+
+        WebElement element = driver.findElement(By.xpath("//a[@class = \"chakra-link css-1hxq3ev\"]"));
+
+        String hrefToLogIn = element.getAttribute("href");
+
+        System.out.println(hrefToLogIn);
+
+        driver.get(hrefToLogIn);
+
+        //Через element.click(); почему-то не работает, пока что не разобрался
     }
 }
